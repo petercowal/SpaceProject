57d3f45f4a13aae1591a7cec4f300f2362a2fe60
Peter Cowal <pcowal@gmail.com>
2016-11-11 20:49:13 -0800

    Adds organization folders, some basic assets
    Let's hope this works

---
+++ SpaceProject/Assets/Scripts/FlightController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlightController : MonoBehaviour {
+	public float rotationRate;
+	public float accelerationRate;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
