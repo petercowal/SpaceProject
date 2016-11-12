d1977e6ec62a68a5e0bb50d7fea0c36087750306
sgrunwald96 <sgrunwald96@gmail.com>
2016-11-11 23:25:16 -0800

    Asteroid Explosions updated

---
+++ Peter&SoniaTakeSpace/Assets/Scripts/destroyController2.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyController2 : MonoBehaviour {
+	int NUM_PIECES = 8;
+	public GameObject asteroid;
+	GameObject clone;
+	Rigidbody rb;
+	int scaleFactor = 8; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space))
+		{
+			Destroy(gameObject);
+			for(int i = 0; i < NUM_PIECES; i++) {
+				clone = (GameObject)Instantiate(asteroid, Random.insideUnitSphere * 2, transform.rotation);
+				clone.GetComponent<Rigidbody>().velocity = Random.insideUnitSphere * scaleFactor;
+			}
+		}
+
+
+	}
+}
+	
